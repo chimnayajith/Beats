6f5091a5fc42be33d6c74082c6de3dc4637d3587
chimnayajith <chinmayajith30@gmail.com>
2023-05-29 10:16:11 +0530

    changelog update for v3.0.1

--- slash/core/changelog.js
+++ slash/core/changelog.js
@@ -11,9 +11,9 @@ module.exports = {
   execute(client, message) {
     const embed1 = new EmbedBuilder()
       .setColor("#2f3136")
-      .setTitle("Changelog")
+      .setTitle("Changelog - [v3.0.1] ")
       .setDescription(
-        `\`\`\`diff\n+ Version Update. \n\n+ Vote on discordbotlist.com to access vote restricted commands.\n\n+ You can now bypass vote retricted commands for a month by becoming a patron.\n\n+ Stability fixes.\n\n- removedupes command is not available due to a package error.\`\`\``
+        `\`\`\`diff\n+ AutoPlay command is now a separate command.\n\n+ UI changes on our website/n/n+ New Blog page : https://blog.beatsbot.in\n\n+ Vote on discordbotlist.com to access vote restricted commands.\n\n+ You can now bypass vote retricted commands for a month by becoming a patron.\n\n+ Stability fixes.\n\n- review page removed from dashboard.\n\n- removedupes command is not available due to a package error.\`\`\``
       );
     message.reply({
       embeds: [embed1],
