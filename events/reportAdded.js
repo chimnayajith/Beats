509768d86462886dfd29b48f77956fb793d22ee3
chimnayajith <chinmayajith30@gmail.com>
2023-07-26 16:03:59 +0530

    reports broadcastEval => webhooks

--- events/reportAdded.js
+++ events/reportAdded.js
@@ -1,18 +1,14 @@
-const {EmbedBuilder} =require("discord.js")
+const {EmbedBuilder , WebhookClient} =require("discord.js")
 module.exports = async (client, user , message ,id) => {
-const channelId = '1112630149297483917';
 
-const newreport = new EmbedBuilder()
+const newReport = new EmbedBuilder()
     .setColor("#2f3136")
     .setTitle("New Report")
     .setAuthor({iconURL : user.avatarURL(),name : `${user.tag} (${user.id})` })
     .setDescription(`${message}\n\n[Click here](https://admin.beatsbot.in/reports/${id})`)
     .setThumbnail('https://cdn.beatsbot.in/Beats.png')
 
-client.shard.broadcastEval(async(client, targetChannelId, messageContent) => {
-  client.channels.fetch(targetChannelId.targetChannelId).then((channel)=>{
-    channel.send({content : '<@&890103815121043466>',embeds : [targetChannelId.messageContent]});
-  }) 
-}, { context: { targetChannelId: channelId, messageContent: newreport } });
+    const webhookClient = new WebhookClient({url : "https://discord.com/api/webhooks/1124816167861751828/KIjR_ic0kMo4StMl-iuzyHXrSPwcIzXVSFoyKu8Oo0Ti2qUDcmj9Iie2bGwOFCAxKwkD"});
+    webhookClient.send({content : '<@&890103815121043466>' , embeds : [newReport]});
 
 }
