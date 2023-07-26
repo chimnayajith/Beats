48a2e08b836838399b825f27c6ff2b63b821e032
chimnayajith <chinmayajith30@gmail.com>
2023-07-26 16:06:05 +0530

    message deleted on trackEnd and queue end

---
+++ player-events/playerFinish.js
@@ -0,0 +1,6 @@
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder , StringSelectMenuBuilder ,WebhookClient} = require("discord.js");
+
+//Track End
+module.exports = async (queue , track) => {
+    queue.metadata.playMessage.delete().catch(console.error);
+};
---
+++ player-events/queueDelete.js
@@ -0,0 +1,4 @@
+//Queue Delete
+module.exports = async (queue) => {
+    queue.metadata.playMessage.delete().catch(console.error);
+};
