bf89b18a32c613ce45d05c6a9042684ca4285365
chimnayajith <chinmaajith30@gmail.com>
2024-12-31 21:41:20 +0530

    fix: use deezer extractor

--- src/common/factory/createPlayer.js
+++ src/common/factory/createPlayer.js
@@ -1,6 +1,6 @@
 const { Player } = require("discord-player");
-const { YoutubeiExtractor } = require("discord-player-youtubei");
-
+// const { YoutubeiExtractor } = require("discord-player-youtubei");
+const { DeezerExtractor } = require("discord-player-deezer");
 module.exports = async () => {
   try {
     const player = new Player(client, {
@@ -11,27 +11,14 @@ module.exports = async () => {
     player
       .on("debug", console.log)
       .events.on("debug", (_, m) => console.log(m));
-    await player.extractors.register(YoutubeiExtractor, {
-      authentication:
-        "access_token=ya29.a0AXooCgvO-dKrD_hs2MER8XHwiOGCfxaLphgFNq-yayf3nITYkhiOGwbmRONBaf9JjkqEmAjhDdlCRo9DfHjwZxexL9tGfjh9atz4PAiJuWdzDO09a8vsoffVV3Lj0fiY58ofGVArcVpaNxodN_t9ILlAfDnhioJEBjD_2uilLBk12vleaCgYKAX8SARESFQHGX2Mi0isNIZcXQIJCQl32u_4aIg0183; refresh_token=1//09dEc9RWLnFWeCgYIARAAGAkSNwF-L9IrbHf07LXieEmp1caBGmLl3AAy1gnpGWC2E17sCqP7fqV3MTp2vqs9o4paPw6lL7rGeBY; scope=https://www.googleapis.com/auth/youtube-paid-content https://www.googleapis.com/auth/youtube; token_type=Bearer; expiry_date=2024-07-31T23:43:26.614Z",
-      /*
-			rotator: {
-				rotationStrategy: "shard",
-				authentications: getAuthArrayFromEnv() || [""],
-				currentShard: client.shard?.ids[0] || 0,
-			},
-			*/
-      streamOptions: {
-        useClient: "ANDROID",
-      },
-    });
-
     global.player = player;
-
+    await player.extractors.register(DeezerExtractor, {
+      decryptionKey: "g4el58wc0zvf9na1",
+    });
     await player.extractors.loadDefault(
       (ext) => !["YouTubeExtractor"].includes(ext),
     );
-
+    global.player = player;
     return player;
   } catch (error) {
     console.log("Failed to create  discord-player player");
